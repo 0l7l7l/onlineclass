b0bc839ff29dde0d5b67fd974866ae56ae82ccbf
445215 柳 知恩 <jieun.yu@rohm.co.jp>
2026-08-21 16:56:11 +0900

    중회화 12과추가

---
+++ 학습지/중국어회화/cn.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+	// 1. 페이지 번호 자동 계산 기능
+	const allPages = document.querySelectorAll(".page");
+	const totalPages = allPages.length;
+
+	allPages.forEach((page, index) => {
+		const pageNum = index + 1;
+		const footer = page.querySelector(".footer");
+		if (footer) {
+			const label = footer.getAttribute("data-label") || "";
+			footer.textContent = `semo.${pageNum} `;
+		}
+	});
+});
+
+
+function toggleAnswers(button) {
+    const page = button.closest('.page');
+    const answers = page.querySelectorAll('.quiz-answer');
+
+    let isShowing = false;
+
+    answers.forEach((answer) => {
+        answer.classList.toggle('show');
+
+        if (answer.classList.contains('show')) {
+            isShowing = true;
+        }
+    });
+
+    button.textContent = isShowing ? '△' : '▽';
+}
